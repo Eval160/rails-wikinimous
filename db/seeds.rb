63d834a08e94c677e875826b843782d0c5ece41f
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2020-08-08 23:55:34 +0200

    crud

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+puts "Destroy all articles"
+Article.destroy_all
+
+puts "Create 10 articles"
+10.times do
+  title = Faker::Lorem.sentence(word_count: 3, supplemental: false, random_words_to_add: 5)
+  content = Faker::Lorem.paragraph(sentence_count: 5, supplemental: false, random_sentences_to_add: 10)
+  new_article = Article.new(title: title, content: content)
+  new_article.save!
+end
+
+puts "Finish!"
